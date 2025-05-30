b382d271025ebd21fac41f3abb8424c2b277a796
Brandon Roberts <robertsbt@gmail.com>
2025-05-29 20:44:59 -0500

    feat: upgrade to Storybook 9.0.0

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,12 +2,7 @@ import { StorybookConfig } from '@analogjs/storybook-angular';
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm"
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-docs"],
   framework: {
     name: "@analogjs/storybook-angular",
     options: {},
