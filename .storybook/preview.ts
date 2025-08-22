31ab41cbccbfddc20c2e299083423c04a00759e7
Brandon Roberts <robertsbt@gmail.com>
2025-08-22 15:25:33 -0500

    chore: update to Analog 1.20.1

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,3 @@
-import 'zone.js';
 import { type Preview } from '@analogjs/storybook-angular';
 import { setCompodocJson } from '@storybook/addon-docs/angular';
 import docJson from '../documentation.json';
